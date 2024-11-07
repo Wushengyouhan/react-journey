c08a827d685b57871c1b0ed5af91b4624c474be2
Wushengyouhan <840835067@qq.com>
2024-11-07 22:59:44 +0800

    feat: 21. Passing Data to Components with Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import Post from "./components/Post";
 function App() {
   return (
     <main>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post author="李白" body="床前明月光，疑是地上霜"/>
+      <Post author="李商隐" body="庄生晓梦迷蝴蝶， 望帝春心托杜鹃"/>
     </main>
   );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,8 @@
-const names = ['哇哒', '啵哒']
-function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1]
+function Post(props) {
   return (
     <div>
-      <p>{chosenName}</p>
-      <p>React.js is Awesome!</p>
+      <p>{props.author}</p>
+      <p>{props.body}</p>
     </div>
   )
 }
