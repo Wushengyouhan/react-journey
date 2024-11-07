bc86281eec4f9823b1d5a762feb583ef5718a6c8
Wushengyouhan <840835067@qq.com>
2024-11-07 22:33:08 +0800

    feat: 19. Outputting Dynamic Values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,9 @@
+const names = ['哇哒', '啵哒']
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1]
   return (
     <div>
-      <p>Chen Chen</p>
+      <p>{chosenName}</p>
       <p>React.js is Awesome!</p>
     </div>
   )
